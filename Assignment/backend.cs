fe286f86e649dff20d4ffc15e2f68560c6600863
Nicholas <amperific@gmail.com>
2015-11-26 20:59:46 +0800

    Backend: Initial Commit
    Method for Product Text Loading

---
+++ Assignment/backend.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class backend
+    {
+
+        //Product Information Loader from Text
+        public void prodinfoText(String location)
+        {
+            string[] lineparsed = System.IO.File.ReadAllLines(@location);
+
+            foreach (string line in lineparsed)
+            {
+                //Object Creation initializes here
+            }
+        }
+
+        //
+    }
+}
