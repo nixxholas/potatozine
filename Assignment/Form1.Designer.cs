2ee6db38004f78e8a8a2882edae95073a1788600
QixyQix <QixyQix@outlook.com>
2015-11-28 12:45:19 +0800

    Added product information windows form

--- Assignment/Form1.Designer.cs
+++ Assignment/Form1.Designer.cs
@@ -28,35 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picBanner = new System.Windows.Forms.PictureBox();
             this.lstCatergory = new System.Windows.Forms.ListBox();
             this.lblCategory = new System.Windows.Forms.Label();
             this.btnLstItms = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.lblPotatoZine = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            this.pictureBox7 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.picItem1 = new System.Windows.Forms.PictureBox();
+            this.picItem2 = new System.Windows.Forms.PictureBox();
+            this.picItem3 = new System.Windows.Forms.PictureBox();
+            this.picItem6 = new System.Windows.Forms.PictureBox();
+            this.picItem5 = new System.Windows.Forms.PictureBox();
+            this.picItem4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBanner)).BeginInit();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem4)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // picBanner
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 59);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(956, 258);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.picBanner.Location = new System.Drawing.Point(12, 59);
+            this.picBanner.Name = "picBanner";
+            this.picBanner.Size = new System.Drawing.Size(956, 258);
+            this.picBanner.TabIndex = 0;
+            this.picBanner.TabStop = false;
             // 
             // lstCatergory
             // 
@@ -113,82 +113,82 @@
             this.lblPotatoZine.TabIndex = 0;
             this.lblPotatoZine.Text = "PotatoZine";
             // 
-            // pictureBox2
+            // picItem1
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(256, 364);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(221, 291);
-            this.pictureBox2.TabIndex = 5;
-            this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.picItem1.Location = new System.Drawing.Point(256, 364);
+            this.picItem1.Name = "picItem1";
+            this.picItem1.Size = new System.Drawing.Size(221, 291);
+            this.picItem1.TabIndex = 5;
+            this.picItem1.TabStop = false;
+            this.picItem1.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
-            // pictureBox3
+            // picItem2
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(493, 364);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(221, 291);
-            this.pictureBox3.TabIndex = 6;
-            this.pictureBox3.TabStop = false;
+            this.picItem2.Location = new System.Drawing.Point(493, 364);
+            this.picItem2.Name = "picItem2";
+            this.picItem2.Size = new System.Drawing.Size(221, 291);
+            this.picItem2.TabIndex = 6;
+            this.picItem2.TabStop = false;
             // 
-            // pictureBox4
+            // picItem3
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(735, 364);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(221, 291);
-            this.pictureBox4.TabIndex = 7;
-            this.pictureBox4.TabStop = false;
+            this.picItem3.Location = new System.Drawing.Point(735, 364);
+            this.picItem3.Name = "picItem3";
+            this.picItem3.Size = new System.Drawing.Size(221, 291);
+            this.picItem3.TabIndex = 7;
+            this.picItem3.TabStop = false;
             // 
-            // pictureBox5
+            // picItem6
             // 
-            this.pictureBox5.Location = new System.Drawing.Point(735, 672);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(221, 291);
-            this.pictureBox5.TabIndex = 10;
-            this.pictureBox5.TabStop = false;
+            this.picItem6.Location = new System.Drawing.Point(735, 672);
+            this.picItem6.Name = "picItem6";
+            this.picItem6.Size = new System.Drawing.Size(221, 291);
+            this.picItem6.TabIndex = 10;
+            this.picItem6.TabStop = false;
             // 
-            // pictureBox6
+            // picItem5
             // 
-            this.pictureBox6.Location = new System.Drawing.Point(493, 672);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(221, 291);
-            this.pictureBox6.TabIndex = 9;
-            this.pictureBox6.TabStop = false;
+            this.picItem5.Location = new System.Drawing.Point(493, 672);
+            this.picItem5.Name = "picItem5";
+            this.picItem5.Size = new System.Drawing.Size(221, 291);
+            this.picItem5.TabIndex = 9;
+            this.picItem5.TabStop = false;
             // 
-            // pictureBox7
+            // picItem4
             // 
-            this.pictureBox7.Location = new System.Drawing.Point(256, 672);
-            this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(221, 291);
-            this.pictureBox7.TabIndex = 8;
-            this.pictureBox7.TabStop = false;
+            this.picItem4.Location = new System.Drawing.Point(256, 672);
+            this.picItem4.Name = "picItem4";
+            this.picItem4.Size = new System.Drawing.Size(221, 291);
+            this.picItem4.TabIndex = 8;
+            this.picItem4.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(980, 1045);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox6);
-            this.Controls.Add(this.pictureBox7);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.picItem6);
+            this.Controls.Add(this.picItem5);
+            this.Controls.Add(this.picItem4);
+            this.Controls.Add(this.picItem3);
+            this.Controls.Add(this.picItem2);
+            this.Controls.Add(this.picItem1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnLstItms);
             this.Controls.Add(this.lblCategory);
             this.Controls.Add(this.lstCatergory);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.picBanner);
             this.Name = "Form1";
             this.Text = "Catalogue";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBanner)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -196,18 +196,18 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picBanner;
         private System.Windows.Forms.ListBox lstCatergory;
         private System.Windows.Forms.Label lblCategory;
         private System.Windows.Forms.Button btnLstItms;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lblPotatoZine;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.PictureBox pictureBox6;
-        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox picItem1;
+        private System.Windows.Forms.PictureBox picItem2;
+        private System.Windows.Forms.PictureBox picItem3;
+        private System.Windows.Forms.PictureBox picItem6;
+        private System.Windows.Forms.PictureBox picItem5;
+        private System.Windows.Forms.PictureBox picItem4;
     }
 }
 
---
+++ Assignment/ItemInfo.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Assignment
+{
+    partial class ItemInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblPotatoZine = new System.Windows.Forms.Label();
+            this.lblPdctInfo = new System.Windows.Forms.Label();
+            this.lblProdNam = new System.Windows.Forms.Label();
+            this.txtProdDesc = new System.Windows.Forms.TextBox();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAddCart = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 117);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(288, 353);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Controls.Add(this.btnBack);
+            this.panel1.Controls.Add(this.lblPotatoZine);
+            this.panel1.Location = new System.Drawing.Point(-7, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(994, 51);
+            this.panel1.TabIndex = 5;
+            // 
+            // lblPotatoZine
+            // 
+            this.lblPotatoZine.AutoSize = true;
+            this.lblPotatoZine.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPotatoZine.ForeColor = System.Drawing.Color.White;
+            this.lblPotatoZine.Location = new System.Drawing.Point(16, 8);
+            this.lblPotatoZine.Name = "lblPotatoZine";
+            this.lblPotatoZine.Size = new System.Drawing.Size(137, 38);
+            this.lblPotatoZine.TabIndex = 0;
+            this.lblPotatoZine.Text = "PotatoZine";
+            // 
+            // lblPdctInfo
+            // 
+            this.lblPdctInfo.AutoSize = true;
+            this.lblPdctInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPdctInfo.Location = new System.Drawing.Point(12, 66);
+            this.lblPdctInfo.Name = "lblPdctInfo";
+            this.lblPdctInfo.Size = new System.Drawing.Size(262, 32);
+            this.lblPdctInfo.TabIndex = 6;
+            this.lblPdctInfo.Text = "Product Information";
+            // 
+            // lblProdNam
+            // 
+            this.lblProdNam.AutoSize = true;
+            this.lblProdNam.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProdNam.Location = new System.Drawing.Point(320, 117);
+            this.lblProdNam.Name = "lblProdNam";
+            this.lblProdNam.Size = new System.Drawing.Size(167, 29);
+            this.lblProdNam.TabIndex = 7;
+            this.lblProdNam.Text = "Product Name";
+            // 
+            // txtProdDesc
+            // 
+            this.txtProdDesc.Location = new System.Drawing.Point(325, 149);
+            this.txtProdDesc.Multiline = true;
+            this.txtProdDesc.Name = "txtProdDesc";
+            this.txtProdDesc.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtProdDesc.Size = new System.Drawing.Size(643, 279);
+            this.txtProdDesc.TabIndex = 8;
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuantity.Location = new System.Drawing.Point(492, 440);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(85, 25);
+            this.lblQuantity.TabIndex = 9;
+            this.lblQuantity.Text = "Quantity";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(583, 435);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 34);
+            this.numericUpDown1.TabIndex = 11;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(320, 440);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(56, 25);
+            this.lblPrice.TabIndex = 12;
+            this.lblPrice.Text = "Price";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(382, 435);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 34);
+            this.textBox1.TabIndex = 13;
+            // 
+            // btnAddCart
+            // 
+            this.btnAddCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddCart.Location = new System.Drawing.Point(809, 434);
+            this.btnAddCart.Name = "btnAddCart";
+            this.btnAddCart.Size = new System.Drawing.Size(159, 36);
+            this.btnAddCart.TabIndex = 14;
+            this.btnAddCart.Text = "Add to cart";
+            this.btnAddCart.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(803, 8);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(172, 33);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "Back to Catalogue";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // ItemInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 491);
+            this.Controls.Add(this.btnAddCart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.txtProdDesc);
+            this.Controls.Add(this.lblProdNam);
+            this.Controls.Add(this.lblPdctInfo);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "ItemInfo";
+            this.Text = "Item Info";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label lblPotatoZine;
+        private System.Windows.Forms.Label lblPdctInfo;
+        private System.Windows.Forms.Label lblProdNam;
+        private System.Windows.Forms.TextBox txtProdDesc;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAddCart;
+    }
+}
