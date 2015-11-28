bba71a0595ce35664db6ec10b833c9a49182637c
Nicholas <amperific@gmail.com>
2015-11-28 16:32:44 +0800

    StreamReader: Initial Commit

--- Assignment/backend.cs
+++ Assignment/backend.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Assignment
 {
@@ -16,6 +17,8 @@ namespace Assignment
 
             foreach (string line in lineparsed)
             {
+                
+
                 //Object Creation initializes here
                 /*
                 Product cosmopolitan = new Product(13.79, "Cosmopolitan", 1);
@@ -27,6 +30,37 @@ namespace Assignment
                 Product[] ProductArr = new Product[3] { new Product(13.79, "Cosmopolitan", 0),
                 new Product(50.60, "Archi Daily", 1),new Product(14.60, "Travelz", 2)};
             }
+        }
+
+        public void LoadDesc()
+        {
+            int counter = 0;
+            string liner;
+
+            System.IO.StreamReader file = new System.IO.StreamReader("c:\\loadprod.txt");
+            while ((liner = file.ReadLine()) != null)
+            {
+                //Console.WriteLine(line);
+                //Object Properties Creator and Inserter
+                counter++;
+            }
+
+            file.Close();
+
+        }
+
+        public void testProdDesc()
+        {
+            System.IO.StreamReader theFile = new System.IO.StreamReader("C:\\loadprod.txt");
+            String DescString = theFile.ReadToEnd();
+
+            theFile.Close();
+
+            /**
+            Custom File Loader
+            Displays all contents
+            **/
+            Console.WriteLine(DescString);
         }
     }
 }
