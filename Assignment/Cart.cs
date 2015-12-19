d449e0e33378a54ceb9ba42396d43d6b2c99d077
QixyQix <QixyQix@outlook.com>
2015-12-19 14:58:34 +0800

    Cart class and GUI
    Added cart class using list. Additem and remove item methods.
    
    List item needs fixing. Does not wanna link to the listbox from the
    class.

---
+++ Assignment/Cart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class Cart : Product
+    {
+        List<int> cart = new List<int>();
+
+        public void addItem(int itemcode) {
+            cart.Add(itemcode);
+        }
+        public void removeItem(int itemcode) {
+            cart.Remove(itemcode);
+        }
+
+        public void listCart() {
+            foreach (int a in cart) {
+                lstCart.Items.Add(a);
+            }
+        }
+    }
+}
---
+++ Assignment/CartGUI.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Assignment
+{
+    partial class CartGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBackShop = new System.Windows.Forms.Button();
+            this.lblPotatoZine = new System.Windows.Forms.Label();
+            this.lblCart = new System.Windows.Forms.Label();
+            this.lstCart = new System.Windows.Forms.ListBox();
+            this.btnCartRem = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Controls.Add(this.btnBackShop);
+            this.panel1.Controls.Add(this.lblPotatoZine);
+            this.panel1.Location = new System.Drawing.Point(1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(994, 51);
+            this.panel1.TabIndex = 16;
+            // 
+            // btnBackShop
+            // 
+            this.btnBackShop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBackShop.Location = new System.Drawing.Point(803, 8);
+            this.btnBackShop.Name = "btnBackShop";
+            this.btnBackShop.Size = new System.Drawing.Size(172, 33);
+            this.btnBackShop.TabIndex = 1;
+            this.btnBackShop.Text = "Back to Catalogue";
+            this.btnBackShop.UseVisualStyleBackColor = true;
+            // 
+            // lblPotatoZine
+            // 
+            this.lblPotatoZine.AutoSize = true;
+            this.lblPotatoZine.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPotatoZine.ForeColor = System.Drawing.Color.White;
+            this.lblPotatoZine.Location = new System.Drawing.Point(16, 8);
+            this.lblPotatoZine.Name = "lblPotatoZine";
+            this.lblPotatoZine.Size = new System.Drawing.Size(137, 38);
+            this.lblPotatoZine.TabIndex = 0;
+            this.lblPotatoZine.Text = "PotatoZine";
+            // 
+            // lblCart
+            // 
+            this.lblCart.AutoSize = true;
+            this.lblCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCart.Location = new System.Drawing.Point(20, 65);
+            this.lblCart.Name = "lblCart";
+            this.lblCart.Size = new System.Drawing.Size(135, 32);
+            this.lblCart.TabIndex = 17;
+            this.lblCart.Text = "Your Cart";
+            // 
+            // lstCart
+            // 
+            this.lstCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstCart.FormattingEnabled = true;
+            this.lstCart.ItemHeight = 25;
+            this.lstCart.Location = new System.Drawing.Point(24, 112);
+            this.lstCart.Name = "lstCart";
+            this.lstCart.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lstCart.Size = new System.Drawing.Size(673, 404);
+            this.lstCart.TabIndex = 18;
+            // 
+            // btnCartRem
+            // 
+            this.btnCartRem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCartRem.Location = new System.Drawing.Point(736, 112);
+            this.btnCartRem.Name = "btnCartRem";
+            this.btnCartRem.Size = new System.Drawing.Size(229, 47);
+            this.btnCartRem.TabIndex = 19;
+            this.btnCartRem.Text = "Remove Item";
+            this.btnCartRem.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(736, 469);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(229, 47);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "CheckOut";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // CartGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 548);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCartRem);
+            this.Controls.Add(this.lstCart);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblCart);
+            this.Name = "CartGUI";
+            this.Text = "CartGUI";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBackShop;
+        private System.Windows.Forms.Label lblPotatoZine;
+        private System.Windows.Forms.Label lblCart;
+        private System.Windows.Forms.Button btnCartRem;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.ListBox lstCart;
+    }
+}
