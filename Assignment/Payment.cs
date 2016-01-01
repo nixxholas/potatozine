2442da427de25a590f7d6210a85e2dd2e6541aa5
QixyQix <QixyQix@outlook.com>
2016-01-02 00:46:42 +0800

    Checking fixes
    Fixes to the checking. shld work fine now

--- Assignment/Payment.cs
+++ Assignment/Payment.cs
@@ -63,7 +63,8 @@ namespace Assignment
         public bool checkInput(bool noerror)
         {
             int temp = 0;
-            noerror = int.TryParse(txtCardNo.Text, out temp);
+            float tempfloat = 0;
+            noerror = float.TryParse(txtCardNo.Text, out tempfloat);
             if (noerror == true)
                 noerror = int.TryParse(txtExpMnth.Text, out temp);
             if (noerror == true)
@@ -71,25 +72,27 @@ namespace Assignment
             if (noerror == true)
                 noerror = int.TryParse(txtCVV.Text, out temp);
             if (noerror == true)
-                noerror = (txtCardHolderNam.Text != null);
+                noerror = (txtCardHolderNam.Text != "");
             if (noerror == true)
-                noerror = (txtAdd1.Text != null);
+                noerror = (txtAdd1.Text != "");
             if (noerror == true)
-                noerror = (txtCity.Text != null);
+                noerror = (txtCity.Text != "");
             if (noerror == true)
-                noerror = (txtCountry.Text != null);
+                noerror = (txtCountry.Text != "");
             if (noerror == true)
-                noerror = (txtState.Text != null);
+                noerror = (txtState.Text != "");
             if (noerror == true)
-                noerror = (txtPostal.Text != null);
+                noerror = (txtPostal.Text != "");
+            if (noerror == true)
+                noerror = (txtHP.Text != "");
             if (noerror == false)
             {
                 MessageBox.Show("You have entered invalid information. Please try again.");
-                return false;
+                return noerror;
             }
             else
             {
-                return true;
+                return noerror;
             }
         }
     }
