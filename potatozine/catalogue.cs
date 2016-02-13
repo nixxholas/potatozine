a69008a0c4618aa6f6d48a4415a891d53adc8ecf
amperific <amperific@gmail.com>
2016-02-13 14:35:38 +0800

    Pusi quad

--- potatozine/catalogue.cs
+++ potatozine/catalogue.cs
@@ -14,6 +14,7 @@ namespace potatozine
 {
     public partial class catalogue : Form
     {
+        String memType;
         public catalogue()
         {
             //Loads the splash screen
@@ -28,24 +29,6 @@ namespace potatozine
             Thread.Sleep(3000);
             t.Abort();
         }
-
-        //Method to read the textfile
-        public void LoadDesc()
-        {
-            System.IO.StreamReader theFile = new System.IO.StreamReader(@"loadprod.txt");
-            String line;
-            int counter = 0;
-
-            while ((line = theFile.ReadLine()) != null)
-            {
-                string[] content = line.Split(',');
-                //arrProducts[counter] = new Product(content[0], double.Parse(content[1]), int.Parse(content[2]), content[3], content[4], content[5]);
-                counter++;
-            }
-
-            theFile.Close();
-        }
-        
         
         public void splashStart()
         {
@@ -64,10 +47,7 @@ namespace potatozine
 
         private void loginBtn_Click(object sender, EventArgs e)
         {
-            //using (SqlConnection cn = new SqlConnection("Data Source=NIXH\\SQLEXPRESS;" +
-            //        "database=login;" +
-            //        "integrated security=true"))
-                if (usernameBox.Text != "" & passwordBox.Text != "")
+            if (usernameBox.Text != "" & passwordBox.Text != "")
             {
                 using (SqlConnection conn = new SqlConnection())
                 {
@@ -87,12 +67,12 @@ namespace potatozine
                     }
 
                 }
-            }   else
+            }
+            else
             {
                 MessageBox.Show("Please enter your credentials.");
-            } 
-                    
             }
+        }
 
         private void registerBtn_Click(object sender, EventArgs e)
         {
@@ -108,7 +88,10 @@ namespace potatozine
 
         private void tabs_Selected(object sender, TabControlEventArgs e)
         {
-
+            if (tabs.SelectedTab == tabs.TabPages["Cart"])
+            {
+                
+            }
         }
 
 
